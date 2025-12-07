cf3cc7d6ec1314994330caf4578e9f4fd8b980de
hopedloki <hopedloki@users.noreply.github.com>
2025-12-07 16:45:47 +0800

    Update project with latest changes and build configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.arcaea.pttcalculator',
   appName: 'Arcaea PTT Calculator',
-  webDir: '.',
+  webDir: 'www',
   server: {
     androidScheme: 'https'
   },
